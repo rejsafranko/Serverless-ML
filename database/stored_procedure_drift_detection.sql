f2ec6bbcfa1099f3b33f72cbae9e2b60d5e9041e
Rej <rej.safranko@codxsolutions.hr>
2024-02-19 10:07:30 +0100

    sql refactoring

--- database/stored_procedure_drift_detection.sql
+++ database/stored_procedure_drift_detection.sql
@@ -1,16 +1,20 @@
 -- Stored procedure for drift detection and train lambda triggering. Drift is set to 100 new data rows.
-DELIMITER $$
-CREATE PROCEDURE update_row_counter_and_trigger_train_lambda()
-BEGIN
-    DECLARE current_row_count INT;
+CREATE PROCEDURE update_row_counter_and_trigger_train_lambda () BEGIN DECLARE current_row_count INT;
 
-    SELECT counter INTO current_row_count FROM searches.row_counter
-    SET current_row_count = current_row_count + 1
-    UPDATE searches.row_counter SET counter = current_row_count
+SELECT
+    counter INTO current_row_count
+FROM
+    searches.row_counter
+SET
+    current_row_count = current_row_count + 1
+UPDATE searches.row_counter
+SET
+    counter = current_row_count IF current_row_count = 100 THEN
+UPDATE searches.row_counter
+SET
+    counter 0 CALL mysql.lambda_async (
+        "arn:aws:lambda:eu-west-1:320329586666:function:MlAutocompleteApiStack-DockerTrain937700CB-kB5UQyk6vdCz",
+        "JSON_PAYLOAD"
+    ) END IF;
 
-    IF current_row_count = 100 THEN
-        UPDATE searches.row_counter SET counter 0
-        CALL mysql.lambda_async("arn:aws:lambda:eu-west-1:320329586666:function:MlAutocompleteApiStack-DockerTrain937700CB-kB5UQyk6vdCz", "JSON_PAYLOAD")
-    END IF;
-END$$
-DELIMITER ; 
+END
--- database/trigger_train_lambda.sql
+++ database/trigger_train_lambda.sql
@@ -1,8 +1,1 @@
-DELIMITER $$
-CREATE TRIGGER lambda_triger
-AFTER INSERT ON searches.verified_searches
-FOR EACH ROW
-BEGIN
-    CALL update_row_counter_and_trigger_train_lambda()
-END$$
-DEILIMITER ;
+CREATE TRIGGER lambda_triger AFTER INSERT ON searches.verified_searches FOR EACH ROW BEGIN CALL update_row_counter_and_trigger_train_lambda () END
