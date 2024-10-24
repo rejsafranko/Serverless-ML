459059a6f497ce043555341e15e62a91b614e9af
rej <rej.safranko@codxsolutions.hr>
2024-10-24 15:37:13 +0200

    wandb integration - training process

--- ml-demo-api/image/src/modules/Config.py
+++ ml-demo-api/image/src/modules/Config.py
@@ -1,6 +1,7 @@
 import os
 
 import dotenv
+import wandb
 
 from typing import Dict, Tuple
 
@@ -22,6 +23,7 @@ class Config:
         database_name = os.getenv("AWS_DATABASE_NAME")
         user = os.getenv("AWS_DATABASE_USERNAME")
         password = os.getenv("AWS_DATABASE_PASSWORD")
+        wandb_api_key = os.getenv("WANDB_API_KEY")
         self._environment_variables = {
             "access_key": access_key,
             "secret_key": secret_key,
@@ -29,9 +31,13 @@ class Config:
             "database_name": database_name,
             "user": user,
             "password": password,
+            "wandb_api_key": wandb_api_key,
         }
 
     def configure_infrastructure(self) -> Tuple[ModelRepository, FeatureStorage]:
+        wandb.login(key=self._environment_variables["wandb_api_key"])
+        wandb.init(project="ml-demo")
+
         model_repository = ModelRepository(
             access_key=self._environment_variables["access_key"],
             secret_key=self._environment_variables["secret_key"],
--- ml-demo-api/image/src/train.py
+++ ml-demo-api/image/src/train.py
@@ -1,7 +1,49 @@
+import uuid
+
+import pandas
 import sklearn.linear_model
+import sklearn.metrics
 import sklearn.model_selection
+import wandb
+
+from typing import Dict, Tuple
 
 from .modules.Config import Config
+
+
+def train(
+    model: sklearn.linear_model.LogisticRegression,
+    train_dataset: Dict[str, pandas.DataFrame | str],
+) -> Tuple[sklearn.linear_model.LogisticRegression, Dict[str, str | float]]:
+    param_grid = {
+        "penalty": ["l1", "l2"],
+        "C": [0.001, 0.01, 0.1, 1, 10, 100],
+    }
+
+    grid_search = sklearn.model_selection.GridSearchCV(
+        estimator=model, param_grid=param_grid, cv=5, scoring="accuracy"
+    )
+
+    grid_search.fit(train_dataset["features"], train_dataset["labels"])
+
+    return grid_search.best_estimator_, grid_search.best_params_
+
+
+def evaluate(
+    model: sklearn.linear_model.LogisticRegression,
+    test_dataset: Dict[str, pandas.DataFrame | str],
+) -> Dict[str, float]:
+    predictions = model.predict(test_dataset["features"])
+    accuracy = sklearn.metrics.accuracy_score(test_dataset["labels"], predictions)
+    precision = sklearn.metrics.precision_score(test_dataset["labels"], predictions)
+    recall = sklearn.metrics.recall_score(test_dataset["labels"], predictions)
+    f1 = sklearn.metrics.f1_score(test_dataset["labels"], predictions)
+    return {
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall,
+        "f1_score": f1,
+    }
 
 
 def handler(event, context):
@@ -17,22 +59,22 @@ def handler(event, context):
     try:
         dataset = feature_storage.fetch_all()
 
-        param_grid = {
-            "penalty": ["l1", "l2"],
-            "C": [0.001, 0.01, 0.1, 1, 10, 100],
-        }
-        model = sklearn.linear_model.LogisticRegression(solver="liblinear")
-
-        grid_search = sklearn.model_selection.GridSearchCV(
-            estimator=model, param_grid=param_grid, cv=5, scoring="accuracy"
+        trained_model, best_params = train(
+            model=sklearn.linear_model.LogisticRegression(solver="liblinear"),
+            train_dataset=dataset["train"],
         )
 
-        grid_search.fit(dataset["train"]["features"], dataset["train"]["labels"])
-        best_model = grid_search.best_estimator_
+        model_unique_name = f"logreg-{uuid.uuid4()}.joblib"
+        wandb.log({"model_name": model_unique_name})
+        wandb.log({"best_params": best_params})
+        wandb.log(evaluate(model=trained_model, test_dataset=dataset["test"]))
+        wandb.finish()
 
-        model_repository.save_model(best_model, "ml-demo-models", "logreg.joblib")
+        model_repository.save_model(trained_model, "ml-demo-models", model_unique_name)
 
         return {"statusCode": 200, "body": {"message": "Model training completed."}}
 
     except Exception as e:
-        print("An unexpected error occured: " + str(e))
+        print(f"An error occurred: {str(e)}")
+        wandb.finish(exit_code=1)
+        return {"statusCode": 500, "body": {"message": "Model training failed."}}
