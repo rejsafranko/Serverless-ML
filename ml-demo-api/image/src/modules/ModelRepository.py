aa5fdc13a8fb16438a76bc03dc1529f101c0e334
rej <rej.safranko@codxsolutions.hr>
2024-10-24 16:47:58 +0200

    model download from s3

--- ml-demo-api/image/src/modules/ModelRepository.py
+++ ml-demo-api/image/src/modules/ModelRepository.py
@@ -1,3 +1,4 @@
+import boto3.exceptions
 import joblib
 import tempfile
 
@@ -30,4 +31,18 @@ class ModelRepository:
     def load_model(
         self, bucket_name: str, model_name: str
     ) -> sklearn.linear_model.LogisticRegression:
-        return sklearn.linear_model.LogisticRegression()
+        try:
+            with tempfile.TemporaryDirectory() as temp_dir:
+                temp_model_path = f"{temp_dir}/{model_name}"
+                self._s3_client.download_file(bucket_name, model_name, temp_model_path)
+                with open(temp_model_path, "rb") as model_file:
+                    model: sklearn.linear_model.LogisticRegression = joblib.load(
+                        model_file
+                    )
+                return model
+        except PermissionError:
+            print("You don't have permission to access the S3 bucket.")
+        except boto3.exceptions.S3UploadFailedError:
+            print("Failed to download the model from S3.")
+        except Exception as e:
+            print("An unexpected error occurred:", e)
