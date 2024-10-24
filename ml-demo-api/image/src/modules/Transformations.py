e8d10c508961c49891be153583f919b2147464b5
rej <rej.safranko@codxsolutions.hr>
2024-10-24 13:02:53 +0200

    api transformations module adjusted to match the db one

--- ml-demo-api/image/src/modules/Transformations.py
+++ ml-demo-api/image/src/modules/Transformations.py
@@ -1,13 +1,11 @@
 import pandas
 
-from typing import List
-
 
 class Transformations:
-    def __init__():
+    def __init__(self) -> None:
         pass
 
-    def strip_text(text: str):
+    def strip_text(text: str) -> str:
         return text.strip()
 
     def encode_labels(feature: pandas.Series) -> pandas.Series:
@@ -15,14 +13,22 @@ class Transformations:
             {"Normal": 0, "Bipolar Type-1": 1, "Bipolar Type-2": 2, "Depression": 3}
         ).astype(int)
 
-    def encode_binary_categories(
-        df: pandas.DataFrame, binary_categories: List[str]
-    ) -> pandas.DataFrame:
-        for category in binary_categories:
-            df[category] = (
-                df[category]
-                .apply(lambda x: x.lower())
-                .map({"yes": 1, "no": 0})
-                .astype(int)
-            )
-        return df
+    def encode_binary_categories(self, dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        for category in dataframe.columns:
+            unique_values = dataframe[category].dropna().unique()
+            if len(unique_values) == 2:
+                dataframe[category] = (
+                    dataframe[category]
+                    .apply(lambda x: x.lower())
+                    .map({"yes": 1, "no": 0})
+                    .astype(int)
+                )
+        return dataframe
+
+    def apply_all(self, dataframe: pandas.DataFrame) -> pandas.DataFrame:
+        dataframe = dataframe.apply(self.strip_text)
+        dataframe = self.encode_binary_categories(dataframe)
+        dataframe["Expert Diagnose"] = dataframe["Expert Diagnose"].apply(
+            self.encode_labels
+        )
+        return dataframe
